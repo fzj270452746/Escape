6f0100fa86715c18e828e3f5430071f317617866
jduja <jduja681@gmail.com>
2025-10-17 20:28:33 +0800

    wee

--- Escape/Escape/SceneDelegate.swift
+++ Escape/Escape/SceneDelegate.swift
@@ -1,5 +1,6 @@
 
 import UIKit
+import AppTrackingTransparency
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -28,6 +29,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            ATTrackingManager.requestTrackingAuthorization {_ in }
+        }
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
